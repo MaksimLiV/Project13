2098563f80e06db1a64eacb4d4fe246835a392ec
Maksim Li <maximlee.ml@gmail.com>
2024-12-24 02:01:06 +0200

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Maksim Li on 24/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+}
+
